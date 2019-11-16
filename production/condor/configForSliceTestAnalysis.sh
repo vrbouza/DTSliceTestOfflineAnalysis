5510b5cdc91012d146129df48e8fb8fd37118ae9
Victor Rodriguez Bouza <15350389+vrbouza@users.noreply.github.com>
2019-11-16 12:10:10 +0100

    Improved so that by default it uses 5kEv

--- production/condor/configForSliceTestAnalysis.sh
+++ production/condor/configForSliceTestAnalysis.sh
@@ -24,9 +24,9 @@ if test -f "DTDPGNtuple_run{runno}.root"; then
 fi
 
 if ((size<1000)) ; then
-    echo "Ntuple production failed, retrying with .dat files"
-    cmsRun dtDpgNtuples_slicetest_cfg.py runNumber={runno} nEvents=5000 runOnDat=True inputFolderCentral=/eos/cms/store/t0streamer/Minidaq/A/
-#     cmsRun dtDpgNtuples_slicetest_cfg.py runNumber={runno} runOnDat=True inputFolderCentral=/eos/cms/store/t0streamer/Minidaq/A/
+  echo "Ntuple production failed, retrying with .dat files"
+  cmsRun dtDpgNtuples_slicetest_cfg.py runNumber={runno} nEvents=5000 runOnDat=True inputFolderCentral=/eos/cms/store/t0streamer/Minidaq/A/
+  #cmsRun dtDpgNtuples_slicetest_cfg.py runNumber={runno} runOnDat=True inputFolderCentral=/eos/cms/store/t0streamer/Minidaq/A/
 fi
 
 # python condor/report.py {runno}
